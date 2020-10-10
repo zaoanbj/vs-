3737b002fec0a561a4a2be24f721fd5eead8904c
zoanbj <332792314@qq.com>
2020-10-10 23:29:35 +0800

    two

---
+++ vscode-project-1/hello.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
